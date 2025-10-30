da1c9526446e5cd125bf25ba63f42acce81a6407
liyangau <d@aufomm.com>
2025-10-31 09:57:17 +1100

    add sg region

---
+++ konnect/sg.nix
@@ -0,0 +1,37 @@
+{ ... }:
+let
+  commonLabels = {
+    environment = "dev";
+    team = "candy";
+  };
+in
+{
+  kontfix = {
+    controlPlanes = {
+      sg = {
+        marshmallow = {
+          labels = commonLabels;
+          create_certificate = true;
+          upload_ca_certificate = true;
+        };
+        chocolate = {
+          labels = commonLabels;
+          create_certificate = true;
+          upload_ca_certificate = true;
+        };
+      };
+    };
+    groups = {
+      sg = {
+        dev_team = {
+          members = [
+            "marshmallow"
+            "chocolate"
+          ];
+          generate_token = true;
+          storage_backend = [ "local" ];
+        };
+      };
+    };
+  };
+}
