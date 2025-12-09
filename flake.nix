d6ba08f42cace20f93f16e280f5e4e865b38b85b
liyangau <d@aufomm.com>
2025-12-09 22:49:20 +1100

    Update module version

--- flake.nix
+++ flake.nix
@@ -26,8 +26,7 @@
             pkgs = nixpkgs.legacyPackages.${system};
           }
         );
-      tf_version = "1.13.4";
-
+      tf_version = "terraform-1.13.4";
       terraformConfiguration =
         system:
         kontfix.lib.kontfixConfiguration {
